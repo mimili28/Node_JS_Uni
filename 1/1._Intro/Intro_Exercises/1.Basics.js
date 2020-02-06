35e0202f29f2a865d62777a1496fc43a22ad36a7
Maria <maria_ilieva28@abv.bg>
2020-02-06 09:46:58 +0100

    exercises

---
+++ 1/1._Intro/Intro_Exercises/1.Basics.js
@@ -0,0 +1,11 @@
+//exercise1
+var firstName="Maria";
+var lastName="Ilieva";
+console.log("My first name is ",firstName, "and my last name is", lastName);
+
+//exercise2
+var year="2017";
+var number=1;
+//var newYear = parseInt(year)+number;
+var newYear=+(year)+1;
+console.log(parseInt(newYear));
