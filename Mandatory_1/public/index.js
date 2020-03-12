ef5e636ab4a95d93afb53acfd6336dbf10c73ec4
Maria <maria_ilieva28@abv.bg>
2020-03-13 00:22:40 +0100

    mandatory new update

--- Mandatory_1/public/index.js
+++ Mandatory_1/public/index.js
@@ -1,10 +1,13 @@
-$('.vertical-menu').css({'width': '18%', "background-color": "#eee"});
+$('.vertical-menu').css({'width': '20%', "background-color": "#eee",
+"position": "fixed", "left": "1", "overflow": "auto"});
 $('.vertical-menu a').css({"background-color": "#eee", "color": "black", "display": "block",
-"text-decoration" : "none"});
+"text-decoration" : "none", "align": "left", "padding": "3px"});
 $('.vertical-menu a.active').css({"background-color": "#4CAF50", "color": "white"});
 
 
-$('.text-box h4').css('margin', '0');
+$('.title h4').css('margin', '0');
 $('.top-bar').css({"background-color": "lightgreen", "margin-top": "0"});
 $('.title').css('text-align', 'center');
 $('.image').css('float', 'left');
+
+$('.documentation-text').css({"margin-left": "22%"})
